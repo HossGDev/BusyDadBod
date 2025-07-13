be028305a20cb8843a46371a5d3fd21ecc30974e
HossGDev <stuckinasimulation@protonmail.com>
2025-07-13 15:13:40 -0700

    add icon link to index.html, working on grid.jsx

--- DadBodMod/src/components/Grid.jsx
+++ DadBodMod/src/components/Grid.jsx
@@ -1,5 +1,34 @@
+import { workoutProgram as training__plan } from '../utils/index.js'
+
 export default function Grid() {
+    const isLocked = true 
     return (
-        <>fssfdfe</>
+        <div className="training-grid-plan">
+            {Object.keys(training__plan).map((workout, workoutIndex) => {
+                return (
+                    <button key={workoutIndex}>
+                        <div className='plan-card-header'>
+                            <p>Day {((workoutIndex / 8) <= 1) ? '0' + (workoutIndex +1):
+                            workoutIndex + 1}</p>
+                        </div>
+                        {isLocked ? (
+                            <i className="fa-solid fa-lock"></i>
+                        ) : (
+                            workoutIndex % 3 === 0 ? (
+                                <i className="fa-solid fa-dumbbell"></i>                      
+                            ) : (
+                                workoutIndex % 3 === 1 ? (
+                                    <i className="fa-solid fa-weight-hanging"></i>
+                                ) : (
+                                    <i className="fa-solid fa-bolt"></i>
+                                )
+                        ))}
+                        <div className="plan-card-header">
+                            <h4><b>{type}</b></h4>
+                        </div>                       
+                    </button>
+                )
+            })}
+        </div>
     )
 }
