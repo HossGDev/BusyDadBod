9123a33750b8c1045c0c4006a42de04b22a3d121
HossGDev <stuckinasimulation@protonmail.com>
2025-07-13 13:34:34 -0700

    complete Hero.jsx

---
+++ DadBodMod/src/components/Hero.jsx
@@ -0,0 +1,41 @@
+export default function Hero() {
+
+    return (
+       <>
+        <h5>Complete this training program if you want to... </h5>
+        <ol className="benifits-list">
+            <li>Add a simple work out thats guranteed results to your busy day</li>
+            <li>Sculpt a shredded, stronger version of your dad bod</li>
+            <li>Build confidence in the gym with simple training tips and techniques</li>
+            <li>Don't let dad duties stop you from crushing your gym goals</li>
+        </ol>
+        <h3>The Rules</h3>
+        <p>To complete this program <b>MUST</b> follow these 3 simple rules:</p>
+        <ul className="rules-list">
+            <div className="rule-item">
+                <p><b>Rest</b></p>
+                <p>
+                    Make sure your are taking rest days when necassary they are good for you
+                </p>
+            </div>
+            <div className="rule-item">
+                <p><b>Reps</b></p>
+                <p>
+                    Every rep is a pause rep following a <abbr title="2 seconds down - 2 seconds pause - 2 seconds up">2 - 2 - 2 tempo</abbr>                
+                </p>
+            </div>
+            <div className="rule-item">
+                <p><b>Weight*</b></p>
+                <p>
+                    Use enough weight to challenge yourself but not so much that you can't complete the reps with good form
+                </p>
+            </div>
+        </ul>
+        <small>* The first and second sets shound be done at 75% and 85% of your working weight used for your last 2 sets </small>
+       <h3>The Training Plan</h3>
+       <p>This training program follows the <b>Bro Split</b> method and adheres to this rotation <br/>👇 </p>
+       <p><b><i>Push &rarr; Pull &rarr; Legs &rarr; Repeat</i></b></p>
+       <p>Complete all the programs below and save all your progress along the way ✅</p>
+       </>
+    )
+}
